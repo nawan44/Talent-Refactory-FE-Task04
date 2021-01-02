4ceca7f60c87d57495990fcdf1548110457997cc
nawan44 <rachmat.d.gunawan@gmail.com>
2021-01-02 22:11:17 +0700

    services section

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Clients from './section/clients.jsx';
 import Header from './header.jsx';
 import About from './section/about.jsx';
-import Service from './section/service.jsx';
+import Services from './section/services.jsx';
 import MediaPartner from './section/mediaPartner.jsx';
 import Contact from './section/contact.jsx';
 import SwiperPorto from './swiper.jsx';
@@ -15,7 +15,7 @@ class Home extends React.Component {
             <div >
                 <Header />
                 <About />
-                <Service />
+                <Services />
                 <MediaPartner />
                 <SwiperPorto />
                 <Clients />
--- src/component/section/mediaPartner.jsx
+++ src/component/section/mediaPartner.jsx
@@ -6,7 +6,7 @@ import './style/media-partner.css';
 class MediaPartner extends React.Component {
     render() {
         return (
-            <div className="media-partner">
+            <div id="media-partner">
 
             </div >
         );
