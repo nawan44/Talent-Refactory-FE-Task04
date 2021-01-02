896c60ffa349d57f1a1e982b05deaa6be435c036
nawan44 <rachmat.d.gunawan@gmail.com>
2021-01-03 04:53:07 +0700

    media partner

---
+++ src/component/section/portofolio.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style/portofolio.css';
+
+class Contact extends React.Component {
+    render() {
+        return (
+            <div id="portofolio">
+
+            </div>
+        );
+    }
+}
+
+export default Contact;
