a5a0457fa4781a6da6279968fb0b67fb8184b7a0
nawan44 <rachmat.d.gunawan@gmail.com>
2021-01-04 12:01:19 +0700

    mobile responsive service section

--- src/component/section/services.jsx
+++ src/component/section/services.jsx
@@ -5,35 +5,8 @@ import services1 from '../img/services/1.png';
 import services2 from '../img/services/2.png';
 import services3 from '../img/services/3.png';
 
-const titleServices = {
-    fontFamily: 'Raleway',
-    fontStyle: 'normal',
-    fontWeight: 900,
-    fontSize: '24px',
-    lineHeight: '26px',
-    color: '#ffffff',
-    textAlign: 'left'
-}
-const listServices = {
-    fontFamily: 'Roboto',
-    fontStyle: 'normal',
-    fontWeight: 300,
-    fontSize: '16px',
-    lineHeight: '23px',
-    color: '#ffffff',
-    textAlign: 'left',
-    margin: '5px 0 0 0'
 
-}
-const servicesLeft = {
-    width: '10%',
-    float: 'left'
-}
-const servicesRight = {
-    width: '23%',
-    float: 'left',
 
-}
 const containerServices = {
     width: '100%'
 }
@@ -48,39 +21,39 @@ class Services extends React.Component {
                 <h5>Our Core in Integrated Marketing Services :</h5>
 
                 <div srtyle={containerServices}>
-                    <div style={servicesLeft}>
+                    <div className="services-left">
                         <img src={services1} className="icon-service" />
                     </div>
-                    <div style={servicesRight}>
-                        <h2 style={titleServices}>1. Services 1</h2>
+                    <div className="services-right">
+                        <h2 className="title-services">1. Services 1</h2>
                         <ul>
-                            <li style={listServices}>Marketing Communication</li>
-                            <li style={listServices}>Event Services</li>
-                            <li style={listServices}>Digital Solution</li>
+                            <li className="list-services">Marketing Communication</li>
+                            <li className="list-services">Event Services</li>
+                            <li className="list-services">Digital Solution</li>
 
                         </ul>
                     </div>
-                    <div style={servicesLeft}>
+                    <div className="services-left">
                         <img src={services2} className="icon-service" />
                     </div>
-                    <div style={servicesRight}>
-                        <h2 style={titleServices}>2. Services 2</h2>
+                    <div className="services-right">
+                        <h2 className="title-services">2. Services 2</h2>
                         <ul>
-                            <li style={listServices}>Media Placement</li>
-                            <li style={listServices}>Public Relations</li>
-                            <li style={listServices}>Sales Leverage</li>
+                            <li className="list-services">Media Placement</li>
+                            <li className="list-services">Public Relations</li>
+                            <li className="list-services">Sales Leverage</li>
 
                         </ul>
                     </div>
-                    <div style={servicesLeft}>
+                    <div className="services-left">
                         <img src={services3} className="icon-service" />
                     </div>
-                    <div style={servicesRight}>
-                        <h2 style={titleServices}>3. Services 3</h2>
+                    <div className="services-right">
+                        <h2 className="title-services">3. Services 3</h2>
                         <ul>
-                            <li style={listServices}>Media Activities</li>
-                            <li style={listServices}>Brand Activation</li>
-                            <li style={listServices}>Integrated Film Solution</li>
+                            <li className="list-services">Media Activities</li>
+                            <li className="list-services">Brand Activation</li>
+                            <li className="list-services">Integrated Film Solution</li>
 
                         </ul>
                     </div>
