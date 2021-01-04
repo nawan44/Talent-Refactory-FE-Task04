0f7064f3d6d325ca721792220f36721c63b98899
nawan44 <rachmat.d.gunawan@gmail.com>
2021-01-04 20:11:59 +0700

    mobile responsive swiper

--- src/component/swiper.jsx
+++ src/component/swiper.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import './style/swiper.css';
+import '../style/App.css';
+
 import part1 from './img/portofolio/port1.jpg';
 import part2 from './img/portofolio/port2.jpg';
 import part3 from './img/portofolio/port3.jpg';
@@ -19,36 +21,9 @@ const swip = {
     height: '1000px',
     color: '#ffffff'
 }
-const lineHeader = {
-    margin: '0 auto',
-    textAlign: 'center',
-    marginTop: 10,
-    marginBottom: 10,
-    width: 500,
-    borderColor: '#ef4035'
-}
-const job = {
-    fontFamily: 'Helvetica Neue',
-    fontStyle: 'normal',
-    fontWeight: 900,
-    fontSize: '30px',
-    lineHeight: '30px',
-    color: '#ffffff',
-    textAlign: 'left',
-    margin: '20px 0 30px 0',
-    padding: '10px'
-}
-const desc = {
-    fontFamily: 'Helvetica Neue',
-    fontStyle: 'normal',
-    fontWeight: 300,
-    fontSize: '18px',
-    lineHeight: '25px',
-    color: '#ffffff',
-    textAlign: 'left',
-    margin: '20px 0 30px 0',
-    padding: '10px'
-}
+
+
+
 const SwiperPorto = () => {
     return (
         <div className="swiper-porto">
@@ -64,10 +39,10 @@ const SwiperPorto = () => {
                 onSlideChange={() => console.log('slide change')}
 
             >
-                <SwiperSlide> <img src={part1} />
+                <SwiperSlide> <img src={part1} className="img-swiper" />
                     <div className="description-job">
                         <h2>Job : </h2>
-                        <h3 style={job}>
+                        <h3 className="job">
                             Sponsorship,
                             Media Partner,
                             Events,
@@ -75,10 +50,10 @@ const SwiperPorto = () => {
                             Brand Activities,
                             Sales Leverage
                         </h3>
-                        <hr style={lineHeader} />
+                        <hr className="line-section" />
                         <h2>Brand :
                         </h2>
-                        <h4 style={desc}>
+                        <h4 className="desc">
                             Berniaga.com,
                             Kacang Garuda,
                             Hotels for Roadshow,
@@ -91,75 +66,76 @@ const SwiperPorto = () => {
                             Talkshow,
                             Interview,
                             Road Show
+                            
                             </h4>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide> <img src={part2} />
+                <SwiperSlide> <img src={part2} className="img-swiper" />
                     <div className="description-job">
                         <h2>Job : </h2>
-                        <h3 style={job}>
+                        <h3 className="job">
                             Sponsorship, Media Partner, Events, Media Placement, Digital Campaign,  Brand Activities, Sales Leverage
                         </h3>
-                        <hr style={lineHeader} />
+                        <hr className="line-section" />
                         <h2>Brand :
                         </h2>
-                        <h4 style={desc}>
+                        <h4 className="desc">
                             Yamaha, Pejaten Village, Labels, Restaurants, Clothes, Insurance, 30 Media partners, in 5 cities, Gala Premiere, Press Screening, Nobar,Meet and Greet, Talkshow, Interview, 4 Days Event
                         </h4>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide> <img src={part3} />
+                <SwiperSlide> <img src={part3} className="img-swiper" />
                     <div className="description-job">
                         <h2>Job : </h2>
-                        <h3 style={job}>
+                        <h3 className="job">
                             Sponsorship, Media Placement, Digital Campaign, Brand Activities, Sales Leverage
                         </h3>
-                        <hr style={lineHeader} />
+                        <hr className="line-section" />
                         <h2>Brand :
                         </h2>
-                        <h4 style={desc}>
+                        <h4 className="desc">
                             Huawei, Mercy, Jaguar, Metrox, Gala Premiere, Press Screening, Nobar,Meet and Greet, Talkshow, Interview, Roadshow
                         </h4>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide> <img src={part4} />
+                <SwiperSlide> <img src={part4} className="img-swiper" />
                     <div className="description-job">
                         <h2>Job : </h2>
-                        <h3 style={job}>
+                        <h3 className="job">
                             Sponsorship
                         </h3>
-                        <hr style={lineHeader} />
+                        <hr className="line-section" />
                         <h2>Brand :
                         </h2>
-                        <h4 style={desc}>
+                        <h4 className="desc">
                             Berniaga.com, Kacang Garuda,  Hotels for Roadshow
                         </h4>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide> <img src={part5} />
+                <SwiperSlide> <img src={part5} className="img-swiper" />
                     <div className="description-job">
                         <h2>Job : </h2>
-                        <h3 style={job}>
+                        <h3 className="job">
                             Sponsorship, Media Partner, Events, Media Placement, Digital Campaign, Brand Activities, Sales Leverage
                         </h3>
-                        <hr style={lineHeader} />
+                        <hr className="line-section" />
                         <h2>Brand :
                         </h2>
-                        <h4 style={desc}>
+                        <h4 className="desc">
                             Ayana Hotel and Resort Bali, Rock Bar, Max One, Citilink, Audi
                         </h4>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide> <img src={part6} />
+                <SwiperSlide> <img src={part6} className="img-swiper" />
                     <div className="description-job">
                         <h2>Job : </h2>
-                        <h3 style={job}>
+                        <h3 className="job">
                             Sponsorship, Media Partner, Events, Media Placement, Digital Campaign,  Brand Activities, Sales Leverage
                         </h3>
-                        <hr style={lineHeader} />
+                        <hr className="line-section" />
                         <h2>Brand :
                         </h2>
-                        <h4 style={desc}>
+                        <h4 className="desc">
 
 
 
